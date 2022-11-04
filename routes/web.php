c166a5cd6b7a6c2d0093cae510595688eac6f0ce
廖文忠 <wcliao1962@gmail.com>
2022-11-04 23:10:27 +0800

    0825_u1_p4:更新資料使用save方法

--- routes/web.php
+++ routes/web.php
@@ -19,11 +19,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
 
     $post = Post::find(1);
-    $post->update([
-        'title'=>'updated title',
-        'content'=>'updated content',
-    ]);
-    return 'Updated!';
+    $post->title = 'saved title';
+    $post->content = 'saved content';
+    $post->save();
+    return 'Saved!';
 
 });
 
