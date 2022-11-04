f91264e04955a23a6a012160e9fc91eb97a9cedf
廖文忠 <wcliao1962@gmail.com>
2022-11-04 18:16:27 +0800

    0825_u2_p2: 用save()方法新增資料

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\PostController;
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -15,7 +17,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return redirect(route('posts.index'));
+    //return redirect(route('posts.index'));
+    $post = new Post();
+    $post->title="test_title";
+    $post->content="test_content";
+    $post->save();
+    return 'Saved, OK!';
 });
 
 Route::get('posts',[PostController::class, 'index'])->name('posts.index');
