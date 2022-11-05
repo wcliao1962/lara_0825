9bbd8166f775ebc51f1ad8bf184090d4f80ab71d
廖文忠 <wcliao1962@gmail.com>
2022-11-05 23:12:21 +0800

    0825_u1_p7:  利用comments()擷取comments資料 (II)

--- routes/web.php
+++ routes/web.php
@@ -35,7 +35,7 @@ Route::get('/', function () {
     echo '標題: '.$post->title.'<br>';
     echo '內容: '.$post->content.'<br>';
     echo '--------------------------'.'<br>';
-    $comments = $post->comments()->get();
+    $comments = $post->comments;
     foreach ($comments as $comment){
         echo '留言: '.$comment->content."<br>";
         echo '--------------------------'.'<br>';
