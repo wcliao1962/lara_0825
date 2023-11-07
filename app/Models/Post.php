3530d8cc5201a23242efb277dfabee1a8b735bd1
廖文忠 <wcliao1962@gmail.com>
2023-11-07 22:39:22 +0800

    posts加FK:user_id & 建立User與Post的1對n關係

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
@@ -16,5 +17,10 @@ class Post extends Model
 
     public function comments(){
         return $this->hasMany(Comment::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
     }
 }
