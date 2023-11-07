4278d2ac46213e3dae0433baf74d213f8478dc7a
廖文忠 <wcliao1962@gmail.com>
2023-11-07 22:42:45 +0800

    模擬產生5個users,每一user產生1至6篇posts，每一post產生1至6則留言

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Comment;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,15 +17,27 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        Comment::truncate();
+        Post::truncate();
+        User::truncate();
+
+        foreach(range(1,5) as $item){
+            User::factory()
+                ->has(Post::factory(rand(1,6))
+                    ->hasComments(rand(1,6)))->create();
+        }
+
+//        User::factory(5)
+//            ->has(Post::factory(5)
+//                ->hasComments(5))->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
 
-        $this->call(
-            PostSeeder::class,
-        );
+//        $this->call(
+//            PostSeeder::class,
+//        );
     }
 }
